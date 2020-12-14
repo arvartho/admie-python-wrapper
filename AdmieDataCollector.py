2f733297e8ebda12b65f8a9053205a4de2d276a7
Argirios Vartholomeos <arvartho@gmail.com>
2020-12-14 22:27:59 +0200

    Initial commit
    Initial commit of ADMIE data collection wrapper
    * AdmieDataCollector.py
    * README.md

---
+++ AdmieDataCollector.py
@@ -0,0 +1,147 @@
+import os
+import json
+import time
+import requests
+import argparse
+import datetime
+from tqdm import tqdm
+
+class AdmieDataCollector():
+   def __init__(self):
+      # API query variables
+      self.baseQueryURL = 'https://www.admie.gr/getOperationMarketFilewRange'
+      self.fileInfoURL = 'https://www.admie.gr/getFiletypeInfo'
+      self.all_filetypes = ['AdhocISPResults', 'CurrentLineOutages', 'CurrentProtectionOutages', 
+            'CurrentSubstationOutages', 'DailyAuctionsSpecificationsATC', 'DailyEnergyBalanceAnalysis', 
+            'DayAheadLoadForecast', 'DayAheadRESForecast', 'DayAheadSchedulingRealTimeDeviations', 
+            'DayAheadSchedulingRequirements', 'DayAheadSchedulingUnitAvailabilities', 'Devit', 
+            'Devnor', 'DispatchSchedulingResults', 'ExPostImbalancePricingResults', 'HVCUSTCONS', 
+            'IMBABE', 'InterconnectionsMaintenanceSchedule', 'IntraDayDispatchSchedulingResults', 
+            'ISP1DayAheadLoadForecast', 'ISP1DayAheadRESForecast', 'ISP1ISPResults', 'ISP1Requirements', 
+            'ISP1UnitAvailabilities', 'ISP2DayAheadLoadForecast', 'ISP2DayAheadRESForecast', 'ISP2ISPResults', 
+            'ISP2Requirements', 'ISP2UnitAvailabilities', 'ISP3IntraDayLoadForecast', 'ISP3IntraDayRESForecast', 
+            'ISP3ISPResults', 'ISP3Requirements', 'ISP3UnitAvailabilities', 'ISP4Requirements', 
+            'ISPWeekAheadLoadForecast', 'LTPTRsNominationsSummary', 'MonthlyLoadForecast', 'MonthlyNTC',
+            'MonthlySIPResults', 'ProvisionalLineOutages', 'ProvisionalProtectionOutages', 
+            'ProvisionalSubstationOutages', 'RealTimeSCADAImportsExports', 'RealTimeSCADARES', 
+            'RealTimeSCADASystemLoad', 'recovery_cost', 'ReservoirFillingRate', 'RESMV', 'RESMVLVPROD',
+            'SignificantEvents', 'SYSBOUNDS', 'SystemEstimationsCorrections', 'SystemRealizationSCADA', 
+            'UA_ANALYSIS', 'UnitAvailabilities', 'UnitProduction', 'UnitsMaintenanceSchedule', 
+            'WeekAheadLoadForecast', 'WeekAheadWaterUsageDeclaration', 'YearlyLoadForecast', 
+            'YearlyWaterUsageDeclaration']
+      self.downloadedFiles = {'date': [], 'filepath': [], 'description': []}
+
+      # Initialize argument parser      
+      self.parser = argparse.ArgumentParser(description='Wraps the ADMIE data collection API', prog='ADMIE API Wrapper')
+      self.parser.add_argument('-s','--startDate', type=datetype, help='''Select start date for the query, date format: YYYY-MM-DD''')
+      self.parser.add_argument('-e','--endDate', type=datetype, help='''Select end date for the query, date format: YYYY-MM-DD''')
+      self.parser.add_argument('-d','--destDir', help='''Select directory to save the data''', default='.')      
+      self.parser.add_argument('-t','--type', help='''Select file type from the available file types according to the ADMIE API:''', required=True, 
+            choices=self.all_filetypes + ['info'])
+
+      self.parser.add_argument('--version', action='version', version='%(prog)s  1.0')
+      self.args = self.parser.parse_args()
+      # Apply argument constrains
+      self.checkArgConstrains()
+   
+   # Query process
+   def executeQuery(self,):
+      params = {'dateStart': self.args.startDate,
+                'dateEnd': self.args.endDate,
+                'FileCategory': self.args.type}
+      
+      if 'info' in params['FileCategory']:
+         self.showAllFileTypes()
+      else:
+         req = requests.get(self.baseQueryURL, params=params)
+         self.downloadFiles(req)      
+      
+   # Check argument constrains      
+   def checkArgConstrains(self,):
+      if self.args.startDate or self.args.endDate:
+         if self.args.startDate and not self.args.endDate:
+            self.parser.error('\nATTENTION: -e/--endDate is required when -s/--startDate is set.')
+         if self.args.endDate and not self.args.startDate:
+            self.parser.error('\nATTENTION: -s/--startDate is required when -e/--endDate is set.')
+         if self.args.endDate < self.args.startDate:
+            self.parser.error('\nATTENTION: Start date cannot be after end date')
+            
+
+   # Display all file types and available information
+   def showAllFileTypes(self,):
+      # Fetch request
+      jsonResp = requests.get(self.fileInfoURL).json()
+      print('Available file types:')
+      for element in jsonResp:
+         filetype = element['filetype']
+         time_gate = element['EN'][0]['time_gate']
+
+         publication_frequencyGR = element['GR'][0]['publication_frequency']
+         publication_frequencyEN = element['EN'][0]['publication_frequency']
+
+         data_typeGR = element['GR'][0]['data_type']
+         data_typeEN = element['EN'][0]['data_type']
+
+         print('''
+         * type: "%s"
+            - description: %s %s / %s %s
+            - when: %s
+
+         ''' % (filetype, 
+                publication_frequencyGR, data_typeGR,
+                publication_frequencyEN, data_typeEN,
+                time_gate))
+
+   # Download query results   
+   def downloadFiles(self, req):
+      # Begin counting
+      start_time = time.time() 
+
+      jsonResp = req.json() 
+      # Create destination path
+      destDir = self.args.destDir      
+      if not os.path.exists(destDir):
+         os.makedirs(destDir)
+
+      if len(jsonResp) == 0:
+         print('No results found for request: %s' % req.url)
+      else:
+         print('Starting files downloading...')
+         processBar = tqdm(jsonResp)
+         for element in processBar:            
+            try:
+               # element attributes
+               url = element['file_path']
+               description = element['file_description']
+               
+               # file destination
+               filename = url.split('/')[-1]
+               filepath = os.path.join(destDir, filename)
+               
+               # initiate file request
+               processBar.set_description("  Downloading file: %s, %s" % (filename, description))
+               req = requests.get(url, allow_redirects=True)
+
+               # save file locally
+               with open(filepath, 'wb') as f:
+                  f.write(req.content)
+                  self.downloadedFiles['filepath'].append(filepath)
+                  self.downloadedFiles['description'].append(description)
+                  self.downloadedFiles['date'].append(filename.split('_')[0])
+
+            except Exception as e:
+               print('Error in request: %s' % str(e))
+
+      print("--- Finished in %s seconds ---" % round(time.time() - start_time, 2))
+
+# Handle date type arguments
+def datetype(dateString):
+   try:
+      date = datetime.datetime.strptime(dateString, '%Y-%m-%d')  # accept only dates with specific format
+   except:
+      print('Error for value %s. -s/--startDate and -e/--endDate arguments have to follow the format YYYY-MM-DD' % dateString)
+   return date
+
+if __name__ == "__main__":
+   admie = AdmieDataCollector()
+   admie.executeQuery()
--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# admie-python-wrapper
- A python wrapper for data colloection for ADMIE
+# AdmieDataCollector
+A Python wrapper for the ADMIE data collection API
+
+>[ADMIE](https://www.admie.gr/en/) is Greece's official Independent Power Transmission Operator (IPTO). The mission of ADMIE is the operation, control, maintenance and development of the Hellenic Electricity Transmission System, to ensure the country’s supply with electricity in an adequate, safe, efficient and reliable manner, as well as the operation of the electricity market for transactions outside the Day Ahead Scheduling, pursuant to the principles of transparency, equality and free competition.
+
+ADMIE offers an [official API](https://www.admie.gr/en/market/market-statistics/file-download-api) for downloading data related to the day to day operations of the operator. This wrapper is used to cquery to the ADMIE API and save locally files from the response. 
+
+## Usage
+
+The wrapper is called from any shell with minor dependancies that are covered by Anaconda distribution. 
+
+Here is an example of executing the wrapper:
+
+```
+$ python AdmieDataCollector.py  -s 2020-11-10 -e 2020-11-20 -t ISP1DayAheadLoadForecast -d . 
+```
+
+The line above will download all the files of type `ISP1DayAheadLoadForecast` for dates starting from `2020-11-10` to `2020-11-20` and save them to the current directory.
+
+The following can be passed as arguments to the wrapper:
+* -s | --startDate: starting date for the query with format YYYY-MM-DD
+* -e | --endDate: ending date for the query with format YYYY-MM-DD
+* -d | --destDir: destination directory, where the files will be stored
+* -t | --type: according to the ADMIE API the available file types are the following with the addition of `info` that is used for display purposes:
+
+```['AdhocISPResults', 'CurrentLineOutages', 'CurrentProtectionOutages', 
+'CurrentSubstationOutages', 'DailyAuctionsSpecificationsATC', 'DailyEnergyBalanceAnalysis', 
+'DayAheadLoadForecast', 'DayAheadRESForecast', 'DayAheadSchedulingRealTimeDeviations', 
+'DayAheadSchedulingRequirements', 'DayAheadSchedulingUnitAvailabilities', 'Devit', 
+'Devnor', 'DispatchSchedulingResults', 'ExPostImbalancePricingResults', 'HVCUSTCONS', 
+'IMBABE', 'InterconnectionsMaintenanceSchedule', 'IntraDayDispatchSchedulingResults', 
+'ISP1DayAheadLoadForecast', 'ISP1DayAheadRESForecast', 'ISP1ISPResults', 'ISP1Requirements', 
+'ISP1UnitAvailabilities', 'ISP2DayAheadLoadForecast', 'ISP2DayAheadRESForecast', 'ISP2ISPResults', 
+'ISP2Requirements', 'ISP2UnitAvailabilities', 'ISP3IntraDayLoadForecast', 'ISP3IntraDayRESForecast', 
+'ISP3ISPResults', 'ISP3Requirements', 'ISP3UnitAvailabilities', 'ISP4Requirements', 
+'ISPWeekAheadLoadForecast', 'LTPTRsNominationsSummary', 'MonthlyLoadForecast', 'MonthlyNTC',
+'MonthlySIPResults', 'ProvisionalLineOutages', 'ProvisionalProtectionOutages', 
+'ProvisionalSubstationOutages', 'RealTimeSCADAImportsExports', 'RealTimeSCADARES', 
+'RealTimeSCADASystemLoad', 'recovery_cost', 'ReservoirFillingRate', 'RESMV', 'RESMVLVPROD',
+'SignificantEvents', 'SYSBOUNDS', 'SystemEstimationsCorrections', 'SystemRealizationSCADA', 
+'UA_ANALYSIS', 'UnitAvailabilities', 'UnitProduction', 'UnitsMaintenanceSchedule', 
+'WeekAheadLoadForecast', 'WeekAheadWaterUsageDeclaration', 'YearlyLoadForecast', 
+'YearlyWaterUsageDeclaration']
+```
+
+The `info` value is the only acceptable file type value for the `--type` argument, outside of the ADMIE API, and is used to display information for each acceptable file type value. For instance:
+
+```
+$ python AdmieDataCollector.py -t info
+
+Available file types:
+
+         * type: "AdhocISPResults"
+            - description: Ημερήσια Αποτελέσματα ΔΕΠ / Daily ISP Results
+            - when:
+
+
+
+         * type: "CurrentLineOutages"
+            - description: Ετήσια Τρέχον Πρόγραμμα Συντήρησης Γραμμών Μεταφοράς / Yearly Current Transmission Lines Outage Planning
+            - when:
+
+
+
+         * type: "CurrentProtectionOutages"
+            - description: Ετήσια Τρέχον Πρόγραμμα Συντήρησης Προστασίας / Yearly Current Protection Outage Planning
+            - when:
+
+
+
+         * type: "CurrentSubstationOutages"
+            - description: Ετήσια Τρέχον Πρόγραμμα Συντήρησης Υποσταθμών / Yearly Current Substations  Outage Planning
+            - when:
+
+
+
+         * type: "DailyAuctionsSpecificationsATC"
+            - description: Ημερήσια Διαθέσιμη Ικανότητα Μεταφοράς ATC / Daily ATC
+            - when: 23:00:00
+
+
+
+         * type: "DailyEnergyBalanceAnalysis"
+            - description:  Ενεργειακό Ισοζύγιο /  Energy Balance
+            - when:
+
+
+
+         * type: "DayAheadLoadForecast"
+            - description: 2 φορές την ημέρα και όποτε άλλοτε χρειαστεί Πρόβλεψη Φορτίου / Twice a day and on demand Load Forecast
+            - when: 09:00 και 11:00
+```
+---
+
+## TODO
+* Add option to export data as CSV
+   1. Add handle for the most popular ADMIE file types
+   2. Collect all files in the selected directory according to ADMIE file type
+   3. Open excel files with pandas
+   4. Format and transform data
+   5. Save to CSV
+
+   
